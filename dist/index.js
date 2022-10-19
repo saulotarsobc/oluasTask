8c48b0fb62383b9bad368fd718e2832c340e8f4e
Saulo Costa <saulotarsobc@gmail.com>
2022-10-19 12:16:28 -0300

    :fire:

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-// import bodyParser from "body-parser";
 const users_1 = __importDefault(require("./routes/users"));
 const tasks_1 = __importDefault(require("./routes/tasks"));
 const app = (0, express_1.default)();
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from 'express';
-// import bodyParser from "body-parser";
 import users from "./routes/users";
 import tasks from "./routes/tasks";
 
-
 const app = express();
+
 app.use('/users', users);
 app.use('/tasks', tasks);
 
