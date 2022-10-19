1ceadef545d3b6167d72168e8ddb11001243e603
Saulo Costa <saulotarsobc@gmail.com>
2022-10-19 14:54:46 -0300

    :fire:

--- dist/index.js
+++ dist/index.js
@@ -9,6 +9,4 @@ const tasks_1 = __importDefault(require("./routes/tasks"));
 const app = (0, express_1.default)();
 app.use('/users', users_1.default);
 app.use('/tasks', tasks_1.default);
-app.listen(3000, (event) => {
-    console.log('server running', event);
-});
+app.listen(3000, () => { console.log('server running'); });
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,4 @@ const app = express();
 app.use('/users', users);
 app.use('/tasks', tasks);
 
-app.listen(3000, (event: any) => {
-    console.log('server running', event)
-});
+app.listen(3000, () => { console.log('server running') });
